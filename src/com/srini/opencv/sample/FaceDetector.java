50bb671fe756a44d802e3a1587429429866229f8
rsrini7 <srinivas.enfs@gmail.com>
2015-05-09 19:00:29 +0100

    java doc provided from the original website to track

--- src/com/srini/opencv/sample/FaceDetector.java
+++ src/com/srini/opencv/sample/FaceDetector.java
@@ -1,3 +1,5 @@
+//modified for opencv 3.0 https://blog.openshift.com/day-12-opencv-face-detection-for-java-developers/
+
 package com.srini.opencv.sample;
 
 import org.opencv.core.Core;
@@ -9,7 +11,23 @@ import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
- 
+
+/**The code shown below does the following :
+
+*   It loads the native OpenCV library so that we can use it using Java API.
+
+*   We create an instance of CascadeClassifier passing it the name of the file from which the classifier is loaded.
+
+*   Next we convert the image to a format which the Java API will accept using the Imgcodecs class  (earlier from Highgui). Mat is the OpenCV C++ n-dimensional dense array class.
+
+*   Then we call the detectMultiScale method on the classifier passing it the image and MatOfRect object. After processing, the MatOfRect will have face detections.
+
+*   We iterate over all the face detections and mark the image with rectangles using the Imgproc class (earlier from Core)
+
+*   Finally, we write the image to the output.png file.
+*   
+*   $make -j4 (This is required for linux - to generate java open cv jar) 
+*/
 public class FaceDetector {
  
     public static void main(String[] args) {
--- src/com/srini/opencv/sample/VideoCap.java
+++ src/com/srini/opencv/sample/VideoCap.java
@@ -1,5 +1,8 @@
+//http://www.codeproject.com/Tips/719878/How-to-Use-OpenCV-with-Java-under-Eclipse-IDE
+
 package com.srini.opencv.sample;
 
+//importing required - first
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
@@ -7,25 +10,43 @@ import org.opencv.videoio.VideoCapture;
         
 public class VideoCap {
     public static void main (String args[]){
+    	
+    	//Secondly, we need to load the native library. This command must be executed once per Java process to using native OpenCV methods. If it does not get used, it throws an exception like "UnsatisfiedLink errors."
     	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    	
+    	//The following statement states that the camera variable is initialized with VideoCapture with 0 "Zero" parameter. It means that zero is first device ID. It accesses the first web camera. Video Camera is also initialized with video file, just pass video file path.
+    	//VideoCapture is moved to org.opencv.videoio.VideoCapture from org.opencv.highgui.VideoCapture
     	VideoCapture camera = new VideoCapture(0);
     	
+    	//( or ) VideoCapture camera = new VideoCapture("F\\video.avi"); 
+
+    	//isOpened function is used to check if the binding of the class to a video source that means device index or video source file path was successful or not to use.
     	if(!camera.isOpened()){
     		System.out.println("Error");
     	}
     	else {
+    		//Mat class with two data parts, first the matrix header which contains information such as the size of the matrix and second a pointer to the matrix contains the pixel values. The matrix header size is constant.
     		Mat frame = new Mat();
+    		
+    		//Following is an infinite loop till the frame is not obtained. 
+    		//If frame has been obtained, it breaks the loop. 
+    		//The following loop prints the frame height and width, it also writes the frame to file and break the loop.
     	    while(true){
     	    	if (camera.read(frame)){
     	    		System.out.println("Frame Obtained");
     	    		System.out.println("Captured Frame Width " + 
     	    		frame.width() + " Height " + frame.height());
+    	    		
+    	    		//imwrite is a static method from the Imgcodecs - open cv 3.x (Highgui class - open cv 2.x). 
+    	    		//This method is used to write an image to a file. In the following frame variable is written to camera.jpg file.
     	    		Imgcodecs.imwrite("camera.jpg", frame);
     	    		System.out.println("OK");
     	    		break;
     	    	}
     	    }	
     	}
+
+    	//For closing the video, we need to call its release function.
     	camera.release();
     }
 }   
